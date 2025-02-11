29bcee77365006b8e365ac94d120aab6942d4498
simonmumina <simonmumina2000@gmail.com>
2025-02-11 20:26:01 +0300

    Initial commit

---
+++ data/siteMetadata.js
@@ -0,0 +1,93 @@
+/** @type {import("pliny/config").PlinyConfig } */
+const siteMetadata = {
+  title: 'CodingEasyPeasy',
+  author: 'CodingEasyPeasy',
+  headerTitle: 'CodingEasyPeasy',
+  description:
+    'CodingEasyPeasy is a blog for programmers and mathematicians. It includes quizzes and articles about programming and mathematics that are thoughtfully written, well-explained, and well written.',
+  language: 'en',
+  theme: 'system', // system, dark or light
+  siteUrl: 'http://codingeasypeasy.com/',
+  siteRepo: 'https://github.com/CodingEasyPeasy',
+  siteLogo: '/static/images/logo.png',
+  socialBanner: '/static/images/twitter-card.png',
+  mastodon: 'https://mastodon.social/',
+  email: 'simonmumina2000@gmail.com',
+  github: 'https://github.com/CodingEasyPeasy',
+  x: 'https://x.com/codingeasypeasy',
+  // twitter: 'https://x.com/codingeasypeasy',
+  facebook: 'https://facebook.com',
+  youtube: 'https://youtube.com',
+  linkedin: 'https://www.linkedin.com',
+  threads: 'https://www.threads.net',
+  instagram: 'https://www.instagram.com',
+  locale: 'en',
+  multiauthors: false,
+  analytics: {
+    // If you want to use an analytics provider you have to add it to the
+    // content security policy in the `next.config.js` file.
+    // supports Plausible, Simple Analytics, Umami, Posthog or Google Analytics.
+    googleAnalytics: {
+      googleAnalyticsId: process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID, // e.g. G-XXXXXXX
+    },
+  },
+  newsletter: {
+    // supports mailchimp, buttondown, convertkit, klaviyo, revue, emailoctopus
+    // Please add your .env file and modify it according to your selection
+    // provider: 'buttondown',
+  },
+  iscomments: false,
+  comments: {
+    // If you want to use an analytics provider you have to add it to the
+    // content security policy in the `next.config.js` file.
+    // Select a provider and use the environment variables associated to it
+    // https://vercel.com/docs/environment-variables
+    // provider: 'giscus', // supported providers: giscus, utterances, disqus
+    giscusConfig: {
+      // Visit the link below, and follow the steps in the 'configuration' section
+      // https://giscus.app/
+      repo: process.env.NEXT_PUBLIC_GISCUS_REPO,
+      repositoryId: process.env.NEXT_PUBLIC_GISCUS_REPOSITORY_ID,
+      category: process.env.NEXT_PUBLIC_GISCUS_CATEGORY,
+      categoryId: process.env.NEXT_PUBLIC_GISCUS_CATEGORY_ID,
+      mapping: 'pathname', // supported options: pathname, url, title
+      reactions: '1', // Emoji reactions: 1 = enable / 0 = disable
+      // Send discussion metadata periodically to the parent window: 1 = enable / 0 = disable
+      metadata: '0',
+      // theme example: light, dark, dark_dimmed, dark_high_contrast
+      // transparent_dark, preferred_color_scheme, custom
+      theme: 'light',
+      // theme when dark mode
+      darkTheme: 'transparent_dark',
+      // If the theme option above is set to 'custom`
+      // please provide a link below to your custom theme css file.
+      // example: https://giscus.app/themes/custom_example.css
+      themeURL: '',
+      // This corresponds to the `data-lang="en"` in giscus's configurations
+      lang: 'en',
+    },
+  },
+  search: {
+    provider: 'kbar', // kbar or algolia
+    kbarConfig: {
+      searchDocumentsPath: 'search.json', // path to load documents to search
+    },
+    // provider: 'algolia',
+    // algoliaConfig: {
+    //   // The application ID provided by Algolia
+    //   appId: 'R2IYF7ETH7',
+    //   // Public API key: it is safe to commit it
+    //   apiKey: '599cec31baffa4868cae4e79f180729b',
+    //   indexName: 'docsearch',
+    // },
+  },
+  // formspree support :
+  //if set to false, simple "mailto"
+  // if set to true, get a free account there : https://formspree.io/ and go to read.me doc
+  formspree: false,
+  // waline support
+  iswaline: false,
+  walineServer: '',
+}
+
+module.exports = siteMetadata
---
+++ scripts/generateBlogs.mjs
@@ -0,0 +1,136 @@
+import { writeFileSync, mkdirSync } from 'fs'
+import path from 'path'
+import { GoogleGenerativeAI, HarmCategory, HarmBlockThreshold } from '@google/generative-ai'
+import slugify from 'slugify'
+
+const apiKey = 'AIzaSyDmJyodUSWaU1Ei-ta66FAGZQ14om3Lb8A'
+
+const genAI = new GoogleGenerativeAI(apiKey)
+
+const model = genAI.getGenerativeModel({
+  model: 'gemini-2.0-flash-exp',
+})
+
+const generationConfig = {
+  temperature: 1,
+  topP: 0.95,
+  topK: 40,
+  maxOutputTokens: 8192,
+  responseMimeType: 'text/plain',
+}
+
+const topics = [
+  'Wordpress',
+  'Data Structures',
+  'Algorithms',
+  'Python',
+  'R',
+  'Amazon Web Services (AWS)',
+  'Google Cloud Platform (GCP)',
+  'Microsoft Azure',
+  'Flask',
+  'Django',
+  'Java',
+  'C',
+  'C++',
+  'JavaScript',
+  'AJAX programming language',
+  'Android Development',
+  'SQL',
+  'Data Science',
+  'Machine Learning',
+  'PHP',
+  'Web Development',
+  'IT System Design',
+  'Programming',
+  'HTML',
+  'CSS',
+  'Bootstrap',
+  'Tailwind Css',
+  'React',
+  'React Native',
+  'NextJs',
+  'VueJs',
+  'NuxtJs',
+  'NodeJS',
+  'Express',
+  'IT Q&A',
+  'Computer Science',
+  'IT Puzzle and answer',
+  'Mathematics',
+  'Git',
+  'Github',
+  'Object oriented programming',
+  'Functional programming',
+  'SEO',
+  'any NPM package',
+]
+
+// function shuffleArray(array) {
+//   for (let i = array.length - 1; i > 0; i--) {
+//     const j = Math.floor(Math.random() * (i + 1))
+//     ;[array[i], array[j]] = [array[j], array[i]]
+//   }
+// }
+
+// shuffleArray(topics)
+
+// let t = topics[0]
+
+console.log(topics[topics.length - 1])
+
+const prompt = `
+generate a long, detailed and seo friendly MDX blog post on anything about Javascript for concepts like variables, functions e.t.c, Make sure to choose a specific topic in Javascript and write about it don't generalise the whole Javascript Language.Include code examples where appropriate. The MDX should be ready to be saved to a file and should include title which is optimised for search engines, date, lastmod, tags which are optimised for search engines, draft with default as false, summary which is optimised for search engines, and author as default for example:
+---
+title: 'Title of blog'
+date: '2021-01-12'
+lastmod: '2024-08-16'
+tags: ['next-js', 'tailwind', 'guide']
+draft: false
+summary: 'summary of blog.'
+authors: ['default']
+---
+`
+async function run() {
+  console.log("called")
+  const chatSession = model.startChat({
+    generationConfig,
+    history: [],
+  })
+
+  const result = await chatSession.sendMessage(prompt)
+
+  const obj = result?.response?.text()
+
+  if (obj) {
+    const titleMatch = obj.match(/title: (.+)/)
+    if (!titleMatch) {
+      console.log('Could not extract title from generated content.')
+      return
+    }
+
+    const title = titleMatch[1]
+
+    const slug = slugify(title, { lower: true, strict: true }) + '.mdx'
+
+    const directoryPath = 'data/blog'
+
+    const lines = obj.split('\n')
+    const newContent = lines.slice(1, lines.length - 1).join('\n')
+
+    async function buildBlog() {
+      mkdirSync(directoryPath, { recursive: true })
+      writeFileSync(path.join(directoryPath, slug), newContent)
+    }
+
+    buildBlog()
+    console.log(slug)
+    console.log('Blog generated...')
+  }
+
+  return
+}
+
+setInterval(() => {
+  run();
+}, 10000)
