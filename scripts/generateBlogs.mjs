0113e8c0403f1f73ef6b2fb9db0afdb2db7e844e
simonmumina <simonmumina2000@gmail.com>
2025-02-12 20:59:40 +0300

    Add blog posts

--- scripts/generateBlogs.mjs
+++ scripts/generateBlogs.mjs
@@ -69,7 +69,7 @@ const topics = [
 console.log(topics[topics.length - 1])
 
 const prompt = `
-generate a long, detailed and seo friendly MDX blog post on eslint package. Include code examples where appropriate. The MDX should be ready to be saved to a file and should include title which is optimised for search engines, date, lastmod, tags which are optimised for search engines, draft with default as false, summary which is optimised for search engines, and author as default for example:
+generate a long, detailed and seo friendly MDX blog post on how to integrate Pipedrive in React JS. Include code examples where appropriate. The MDX should be ready to be saved to a file and should include title which is optimised for search engines, date, lastmod, tags which are optimised for search engines, draft with default as false, summary which is optimised for search engines, and author as default for example:
 ---
 title: 'Title of blog'
 date: '2021-01-12'
