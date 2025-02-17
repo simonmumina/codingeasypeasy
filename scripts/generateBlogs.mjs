e5c2bfd1444d68976f044d8cd08e7e6ccd9cd7c6
simonmumina <simonmumina2000@gmail.com>
2025-02-17 23:05:22 +0300

    Add blog posts

--- scripts/generateBlogs.mjs
+++ scripts/generateBlogs.mjs
@@ -69,7 +69,7 @@ const topics = [
 // console.log(topics[topics.length - 1])
 
 const prompt = `
-generate a long, detailed and seo friendly MDX blog post on What is the difference between include and require in PHP?. Include code examples where appropriate. The MDX should be ready to be saved to a file and should include title which is optimised for search engines, date, lastmod, tags which are optimised for search engines, draft with default as false, summary which is optimised for search engines, and author as default for example:
+generate a long, detailed and seo friendly MDX blog post on How to Create ToDo App using React Native?. Include code examples where appropriate. The MDX should be ready to be saved to a file and should include title which is optimised for search engines, date, lastmod, tags which are optimised for search engines, draft with default as false, summary which is optimised for search engines, and author as default for example:
 ---
 title: 'Title of blog'
 date: '2021-01-12'
