773768efc5dad3ff79a1f064e7966a10b02150b5
simonmumina <simonmumina2000@gmail.com>
2025-02-19 18:06:59 +0300

    Add blog posts

--- next.config.js
+++ next.config.js
@@ -13,7 +13,7 @@ const ContentSecurityPolicy = `
   media-src *.s3.amazonaws.com;
   connect-src *;
   font-src 'self' https://fonts.gstatic.com/;
-  frame-src giscus.app
+  frame-src giscus.app https://vercel.live/;
 `
 
 const securityHeaders = [
--- scripts/generateBlogs.mjs
+++ scripts/generateBlogs.mjs
@@ -69,11 +69,11 @@ const topics = [
 // console.log(topics[topics.length - 1])
 
 const prompt = `
-generate a long, detailed and seo friendly MDX blog post on Learn how to incrementally upgrade the Expo SDK version in your project. Include code examples where appropriate. The MDX should be ready to be saved to a file and should include title which is optimised for search engines, date, lastmod, tags which are optimised for search engines, draft with default as false, summary which is optimised for search engines, and author as default for example:
+generate a long, detailed and seo friendly MDX blog post on JavaScript Statements. Include code examples where appropriate. The MDX should be ready to be saved to a file and should include title which is optimised for search engines, date, lastmod, tags which are optimised for search engines, draft with default as false, summary which is optimised for search engines, and author as default for example:
 ---
 title: 'Title of blog'
-date: '2021-01-12'
-lastmod: '2024-08-16'
+date: '2024-10-12'
+lastmod: '2025-01-16'
 tags: ['next-js', 'tailwind', 'guide']
 draft: false
 summary: 'summary of blog.'
