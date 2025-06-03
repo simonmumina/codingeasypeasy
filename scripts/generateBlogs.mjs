0ac36d155bec1c93ac8cbe690a211e82723a4433
simonmumina <simonmumina2000@gmail.com>
2025-06-03 23:18:16 +0300

    Add cmd windows blog posts

--- scripts/generateBlogs.mjs
+++ scripts/generateBlogs.mjs
@@ -67,7 +67,7 @@ const topics = [
 ]
 
 const prompt = `
-generate a long, detailed and seo friendly MDX blog post on What are the alternatives to Spring Boot in Kotlin. Include code examples where appropriate. The MDX should be ready to be saved to a file and should include title which is optimised for search engines, date, lastmod, tags which are optimised for search engines, draft with default as false, summary which is optimised for search engines, and author as default for example:
+generate a long, detailed and seo friendly MDX blog post on How to change the Windows 10/11 login background via CMD windows. Include code examples where appropriate. The MDX should be ready to be saved to a file and should include title which is optimised for search engines, date, lastmod, tags which are optimised for search engines, draft with default as false, summary which is optimised for search engines, and author as default for example:
 ---
 title: 'Title of blog'
 date: '2024-10-12'
