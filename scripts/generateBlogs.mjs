c0668edb1273d01672a098264d34d254ad6717f9
simonmumina <simonmumina2000@gmail.com>
2025-06-14 17:41:14 +0300

    Add more blog posts

--- scripts/generateBlogs.mjs
+++ scripts/generateBlogs.mjs
@@ -67,7 +67,7 @@ const topics = [
 ]
 
 const prompt = `
-generate a long, detailed and seo friendly MDX blog post on What is null safety in Dart?. Include code examples where appropriate. The MDX should be ready to be saved to a file and should include title which is optimised for search engines, date, lastmod, tags which are optimised for search engines, draft with default as false, summary which is optimised for search engines, and author as default for example:
+generate a long, detailed and seo friendly MDX blog post on How to filter/map/iterate collections Dart?. Include code examples where appropriate. The MDX should be ready to be saved to a file and should include title which is optimised for search engines, date, lastmod, tags which are optimised for search engines, draft with default as false, summary which is optimised for search engines, and author as default for example:
 ---
 title: 'Title of blog'
 date: '2024-10-12'
